3817977f256d330f0dde985fee72a9ab15a4bd44
Sylwekqaz <sylwekqaz19@gmail.com>
2017-10-27 14:34:33 +0200

    Add Domain project

---
+++ src/ChatSggw.Domain/DomainEvents.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using Neat.CQRSLite.Contract.Events;
+
+namespace ChatSggw.Domain
+{
+    public static class DomainEvents
+    {
+        private static readonly List<Action<IEvent>> EventHandlers = new List<Action<IEvent>>();
+
+        public static void Publish(IEvent @event)
+        {
+            EventHandlers.ForEach(handler => handler(@event));
+        }
+
+        public static SubscriptionToken RegisterEventHandler(Action<IEvent> handler)
+        {
+            if (handler == null)
+                throw new ArgumentNullException(nameof(handler));
+
+            var token = new SubscriptionToken(handler);
+            EventHandlers.Add(handler);
+            return token;
+        }
+
+        public sealed class SubscriptionToken : IDisposable
+        {
+            private readonly Action<IEvent> _handler;
+
+            public SubscriptionToken(Action<IEvent> handler)
+            {
+                _handler = handler;
+            }
+
+            public void Dispose()
+            {
+                EventHandlers.Remove(_handler);
+                GC.SuppressFinalize(this);
+            }
+        }
+    }
+}
