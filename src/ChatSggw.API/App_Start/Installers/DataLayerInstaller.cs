08435463ee8b7111320cf06a9ff1a8f7a838ef75
Sylwekqaz <sylwekqaz19@gmail.com>
2017-11-10 00:43:33 +0100

    Add castle windsor

---
+++ src/ChatSggw.API/App_Start/Installers/DataLayerInstaller.cs
@@ -0,0 +1,30 @@
+﻿using System.Configuration;
+using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+using ChatSggw.DataLayer;
+using ChatSggw.Domain;
+
+namespace ChatSggw.API.Installers
+{
+    public class DataLayerInstaller : IWindsorInstaller
+    {
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+            var dbSettings = new CoreDbContext.Settings()
+            {
+                ConnectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString,
+            };
+
+            container.Register(
+                Component.For<CoreDbContext>()
+                    .DependsOn(Dependency.OnValue<CoreDbContext.Settings>(dbSettings))
+                    .LifestyleScoped(),
+                Classes.FromAssemblyInThisApplication()
+                    .BasedOn(typeof(IRepository<,>))
+                    .WithService.Base()
+                    .LifestyleScoped()
+            );
+        }
+    }
+}
---
+++ src/ChatSggw.API/Infrastructure/DependencyResolver.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http.Dependencies;
+using Castle.MicroKernel;
+
+namespace ChatSggw.API.Infrastructure
+{
+    internal class DependencyResolver : System.Web.Http.Dependencies.IDependencyResolver
+    {
+        private readonly IKernel _kernel;
+
+        public DependencyResolver(IKernel kernel)
+        {
+            _kernel = kernel;
+        }
+
+        public IDependencyScope BeginScope()
+        {
+            return new DependencyScope(_kernel);
+        }
+
+        public object GetService(Type type)
+        {
+            return _kernel.HasComponent(type) ? _kernel.Resolve(type) : null;
+        }
+
+        public IEnumerable<object> GetServices(Type type)
+        {
+            return _kernel.ResolveAll(type).Cast<object>();
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
