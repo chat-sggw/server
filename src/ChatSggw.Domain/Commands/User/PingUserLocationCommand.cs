6a70860bc5b397e85cd5c43b6c2dd18b50b7ec53
Sylwekqaz <sylwekqaz19@gmail.com>
2017-12-28 21:44:18 +0100

    Add basic command

---
+++ src/ChatSggw.Domain/Commands/User/PingUserLocationCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using ChatSggw.Domain.Structs;
+using Neat.CQRSLite.Contract.Commands;
+
+namespace ChatSggw.Domain.Commands.User
+{
+    public class PingUserLocationCommand : ICommand
+    {
+        public Guid UserId { set; get; }
+        public GeoInformation Location { get; set; }
+    }
+}
