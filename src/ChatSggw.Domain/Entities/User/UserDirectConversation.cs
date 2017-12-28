8e2d6b54b030bc361e4e312ed5d64bd5221674c9
Sylwekqaz <sylwekqaz19@gmail.com>
2017-12-28 15:22:53 +0100

    fix keys for entities

--- src/ChatSggw.Domain/Entities/User/UserDirectConversation.cs
+++ src/ChatSggw.Domain/Entities/User/UserDirectConversation.cs
@@ -1,14 +1,12 @@
 ﻿using System;
-using Neat.CQRSLite.Contract.Domain;
 
 namespace ChatSggw.Domain.Entities.User
 {
-    public class UserDirectConversation : IAggregate
+    public class UserDirectConversation
     {
-        public Guid Id { get; set; }
-        public Guid ConversationId { get; set; }
         public Guid UserId { get; set; }
         public Guid InterlocutorId { get; set; }
+        public Guid ConversationId { get; set; }
         public bool IsFriend { get; set; }
 
     }
