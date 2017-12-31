1263f5fcd943da514db3a7332a430948c4f82977
ka-res <kares.inf@gmail.com>
2017-12-31 14:24:32 +0100

    add sendMessage api mock

--- src/ChatSggw.Domain/Commands/Message/SendMessageCommand.cs
+++ src/ChatSggw.Domain/Commands/Message/SendMessageCommand.cs
@@ -1,9 +1,15 @@
-﻿using Neat.CQRSLite.Contract.Commands;
+﻿using System;
+using ChatSggw.Domain.Structs;
+using Neat.CQRSLite.Contract.Commands;
 
 namespace ChatSggw.Domain.Commands.Message
 {
     public class SendMessageCommand : ICommand
     {
-        public Entities.Conversation.Message Message { get; set; }
+        public Guid MessageId { get; set; }
+        public Guid MemberId { get; set; }
+        public Guid ConversationId { get; set; }
+        public GeoInformation GeoStamp { get; set; }
+        public string Text { get; set; }
     }
 }
